4464058f1b01f66221d6f1d9375e953d10e8c549
devman-3000 <koliamarchuk50@gmail.com>
2023-01-09 00:10:16 +0200

    web api

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id){
+            return await _context.Products.FindAsync(id);
+        }
+
+        [HttpGet("brand/{brand}")]
+        public async Task<ActionResult<List<Product>>> GetProductsByBrand(string brand){
+            
+            return await _context.Products.Where(o => o.Brand == brand).ToListAsync();
+
+        }
+    }
+}
