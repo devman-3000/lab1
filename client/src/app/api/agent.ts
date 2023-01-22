5cc9296f4d30eea542fbebb1d8d7282a6c845039
devman-3000 <koliamarchuk50@gmail.com>
2023-01-23 00:54:51 +0200

    jwt auth backend

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -61,14 +61,6 @@ const Catalog = {
     fetchFilters: () => requests.get('products/filters')
 }
 
-const TestErrors = {
-    get400Error: () => requests.get('buggy/bad-request'),
-    get401Error: () => requests.get('buggy/unauthorised'),
-    get404Error: () => requests.get('buggy/not-found'),
-    get500Error: () => requests.get('buggy/server-error'),
-    getValidationError: () => requests.get('buggy/validation-error'),
-}
-
 const Basket = {
     get: () => requests.get('basket'),
     addItem: (productId: number, quantity = 1) => requests.post(`basket?productId=${productId}&quantity=${quantity}`, {}),
@@ -77,7 +69,6 @@ const Basket = {
 
 const agent = {
     Catalog,
-    TestErrors,
     Basket
 }
 
